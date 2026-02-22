47adee4234e2408ed068720f0dfda91589292ae5
Rajamurugan09 <rajamurugan09123@gmail.com>
2026-02-22 10:57:59 +0530

    feat:Add round2

--- app/main.py
+++ app/main.py
@@ -8,9 +8,8 @@ from app.models import round as round_model
 from app.models import question as question_model
 from app.models import answer as answer_model
 from app.routers import round
-
 from app.routers import candidate, interview
-
+from app.routers import round2_router
 app = FastAPI(
     title="AI Interview System",
     version="1.0.0"
@@ -21,9 +20,11 @@ Base.metadata.create_all(bind=engine)
 app.include_router(candidate.router)
 app.include_router(interview.router)
 app.include_router(round.router)
-
-
+app.include_router(round2_router.router, prefix="/round2", tags=["Round 2"])
 
 @app.get("/")
 def root():
     return {"message": "AI Interview System Backend Running"}
+
+
+
